a76c399c7548ff0f857af27004c6d8bb5f038734
davissears <davisrandsears@gmail.com>
2025-09-22 00:52:28 -0700

    Add container creation and DOM manipulation in events.js

--- events.js
+++ events.js
@@ -1,1 +1,10 @@
-//dom manipulation events
+//* container
+//* 1. create Element
+const container = document.createElement("div");
+//* Container Class & ID
+container.setAttribute("class", "divContainer");
+container.setAttribute("id", "parentContainer");
+//* 2. append
+document.body.appendChild(container);
+//* 3. get reference
+const parentContainer = document.getElementById("parentContainer");
