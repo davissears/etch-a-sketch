61820a7f4074af31618bf393a11ab1730a2e5e2b
davissears <davisrandsears@gmail.com>
2025-09-22 00:58:45 -0700

    Populate container with child divs and set unique IDs for each

--- events.js
+++ events.js
@@ -8,3 +8,17 @@ container.setAttribute("id", "parentContainer");
 document.body.appendChild(container);
 //* 3. get reference
 const parentContainer = document.getElementById("parentContainer");
+
+//* populate Children
+
+//*for loop parameters
+for (let i = 0; i < 256; i++) {
+  var childDiv = document.createElement("div");
+  //appends to container
+  parentContainer.appendChild(childDiv);
+  // className for CSS Styling
+  childDiv.className = "children";
+  // creates unique ID's for each childDiv
+  childDiv.id = `child-div-${i + 1}`;
+  //listens for mouseover event
+}
